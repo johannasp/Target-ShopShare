d5587152239839bb38da4106b3bac7239ae1413b
johannasp <jlsp101@gmail.com>
2017-05-19 17:27:29 -0500

    Index (php)

--- index.php
+++ index.php
@@ -48,27 +48,66 @@
 
 <div class="container">
   <div class="row">
+
     <div class="col-sm-4">
       <div class="panel panel-primary">
         <div class="panel-heading">BLACK FRIDAY DEAL</div>
         <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+        <div class="panel-footer">
+          <div class="panel-footer-price">$30</div>
+          <div class="panel-footer-name">Item Name</div>
+          Buy 50 mobiles and get a gift card
+        </div>
       </div>
     </div>
     <div class="col-sm-4">
       <div class="panel panel-danger">
         <div class="panel-heading">BLACK FRIDAY DEAL</div>
         <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+        <div class="panel-footer">
+          <div class="panel-footer-price">$30</div>
+          <div class="panel-footer-name">Item Name</div>
+          Buy 50 mobiles and get a gift card
+        </div>
       </div>
     </div>
     <div class="col-sm-4">
       <div class="panel panel-success">
         <div class="panel-heading">BLACK FRIDAY DEAL</div>
         <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+        <div class="panel-footer">
+          <div class="panel-footer-price">$30</div>
+          <div class="panel-footer-name">Item Name</div>
+          Buy 50 mobiles and get a gift card
+        </div>
       </div>
     </div>
+
+    <?php
+      $json_data = file_get_contents("data.json");
+      $json = json_decode($json_data);
+      //echo var_dump($json);
+
+      foreach ($json as $object) {
+        $itemName = $object->name;
+        $itemPrice = $object->price;
+        $itemPic = $object->pic;
+        echo "
+        <div class=\"col-sm-4\">
+          <div class=\"panel panel-success\">
+            <div class=\"panel-heading\">ITEM</div>
+            <div class=\"panel-body\"><img src=\"$itemPic\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\"></div>
+            <div class=\"panel-footer\">
+              <div class=\"panel-footer-price\">$itemPrice</div>
+              <div class=\"panel-footer-name\">$itemName</div>
+              Buy 50 mobiles and get a gift card
+            </div>
+          </div>
+        </div>
+        ";
+      }
+    ?>
+
   </div>
 </div><br>
 
