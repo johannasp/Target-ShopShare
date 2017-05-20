0697b2f43072a3a405690e8f71193301ba0ccc1c
johannasp <jlsp101@gmail.com>
2017-05-19 22:43:25 -0500

    Nav file, button

---
+++ nav.php
@@ -0,0 +1,39 @@
+<?php
+  echo "
+    <nav class=\"navbar navbar-inverse\">
+      <div class=\"container-fluid\">
+        <div class=\"navbar-header\">
+          <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#myNavbar\">
+            <span class=\"icon-bar\"></span>
+            <span class=\"icon-bar\"></span>
+            <span class=\"icon-bar\"></span>
+          </button>
+          <a class=\"navbar-brand\" href=\"#\"><img src=\"logo.png\" alt=\"Target logo\" class=\"navbar-logo\"></a>
+        </div>
+        <div class=\"collapse navbar-collapse\" id=\"myNavbar\">
+          <ul class=\"nav navbar-nav\" id=\"navbar\">
+            <li><a href=\"#\">home</a></li>
+            <li><a href=\"#\">products</a></li>
+            <li><a href=\"#\">deals</a></li>
+            <li><a href=\"#\">stores</a></li>
+            <li><a href=\"#\">contact</a></li>
+          </ul>
+          <ul class=\"nav navbar-nav navbar-right\">
+            <li><a href=\"#\" id=\"navbar-right\"><span class=\"glyphicon glyphicon-user\"></span> my account</a></li>
+            <li><a href=\"#\" id=\"navbar-right\"><span class=\"glyphicon glyphicon-shopping-cart\"></span> Cart</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class=\"jumbotron\">
+        <div class=\"container text-center\">
+          <h1>
+            <div class=\"title-target\">TARGET</div>
+            <div class=\"title-subtitle\">ShopShare</div>
+          </h1>
+        </div>
+      </div>
+
+    </nav>
+  ";
+?>
